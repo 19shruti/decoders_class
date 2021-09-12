3142f84671536e0ed3a567acbf314a53904fed42
SHRUTI RAJ <69633187+19shruti@users.noreply.github.com>
2021-09-12 21:48:19 +0530

    Update subarraysum.cpp

--- subarraysum.cpp
+++ subarraysum.cpp
@@ -1,1 +1,22 @@
 
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        //initializing map to check whether temp is present their or not . if yes count++
+        unordered_map<int,int>m;
+        int sum=0;
+        int count=0;
+        m[0]++;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            int temp=sum-k;
+            if(m[temp]!=0)
+                count=count+m[temp];
+                m[sum]++;
+            
+        }
+      return count;
+        
+    }
+};
